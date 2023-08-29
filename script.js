37abf978bec391b5ba46a93b5fae3fcc0330cc18
Poutama M <pouhuntfish@gmail.com>
2023-08-29 21:24:55 +1200

    Finis

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("Connected")
+
+
+
+
+// function getUserName(userName) {
+//     const userName = prompt("What is your name?")
+//     return(userName);
+// }
+// getUserName(); 
+
+// function changeMainColor(color) {
+//     const main = document.querySelector('main');
+//     main.style.backgroundColor = color;
+// }
